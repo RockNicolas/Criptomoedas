4db2290ce04d6d2f93c077afb205f9a7547eec6a
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-01-31 12:28:56 -0300

    Limpando o padrão do react, importando a logo, criação de pastas aonde ficaram os componentes de cada página

---
+++ src/pages/detail/detail.tsx
@@ -0,0 +1,9 @@
+
+export function Detail() {
+    return (
+      <div>
+        <h1>Página Detalhe das moedas</h1>
+      </div>
+    )
+  }
+  
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,11 @@
+
+
+export function Home() {
+
+    return (
+      <div>
+        <h1>Página Home</h1>
+      </div>
+    )
+  }
+  
