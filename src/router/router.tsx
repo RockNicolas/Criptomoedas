62441155f46e81bff5c4576a3278564c691cdeb6
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-01-31 13:06:50 -0300

    Craição do header e do layout , utilização do Outlet para se repetir em todas as paginas com mais facilidade, muito mais pratico em ter que importa em cada página da sua aplicação

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -2,9 +2,11 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Detail } from '../pages/detail/detail'
 import { Home } from '../pages/home/home'
 import { NotFound } from '../pages/notfound/notfound'
+import { Layout } from '../components/layout/layout'
 
 const router = createBrowserRouter([
     {
+        element: <Layout />,
         children: [
             {
                 path: "/",
