537196b336990c9890bd56318e55faf7f06d8cae
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-01-31 13:17:13 -0300

    Trocando o estilo de css do notfound usando module

--- src/pages/notfound/notfound.tsx
+++ src/pages/notfound/notfound.tsx
@@ -1,14 +1,14 @@
 import { Link } from 'react-router-dom';
-import './notfound.css'
+import styles from './notfound.module.css'; 
 
 export function NotFound() {
   return (
-    <div className="notfound-container">
-      <div className="notfound-content">
-        <h1 className="notfound-title">404</h1>
-        <h2 className="notfound-subtitle">Ops, essa página não existe!</h2>
-        <p className="notfound-text">A página que você está procurando pode ter sido movida ou não estar mais disponível.</p>
-        <Link to="/" className="notfound-link">Voltar para a página inicial</Link>
+    <div className={styles.notfoundContainer}>  {}
+      <div className={styles.notfoundContent}>
+        <h1 className={styles.notfoundTitle}>404</h1>
+        <h2 className={styles.notfoundSubtitle}>Ops, essa página não existe!</h2>
+        <p className={styles.notfoundText}>A página que você está procurando pode ter sido movida ou não estar mais disponível.</p>
+        <Link to="/" className={styles.notfoundLink}>Voltar para a página inicial</Link>
       </div>
     </div>
   );
