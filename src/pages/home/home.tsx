55d2cc05f093dd4d60617a519b46f5acc92d13b5
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-01-31 15:47:28 -0300

    Fazendo a funcionabilidade de alguns botões e criando os botões

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,16 +1,32 @@
+import { useState, FormEvent } from 'react'
 import styles from './home.module.css'
 import { BsSearch } from 'react-icons/bs'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export function Home() {
+    const [input, setInput] = useState("")
+    const navigate = useNavigate();
+
+    function handleSubmit(e: FormEvent){
+        e.preventDefault();
+
+        if(input === "") return;
+
+        navigate(`/detail/${input}`)
+    }
+
+    function handdleGetMore(){
+        alert('teste')
+    }
 
     return (
       <main className={styles.container}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
             <input
               type="text"
               placeholder="Digite o noem da moeda... Ex Bitcoin"
-            
+              value={input}
+              onChange={(e) => setInput(e.target.value) }
             />
             <button type="submit">
                 <BsSearch size={30} color='#FFF'/>
@@ -55,6 +71,10 @@ export function Home() {
                 </tr>
             </tbody>
         </table>
+
+        <button className={styles.buttonMore} onClick={handdleGetMore}>
+            Carregar mais
+        </button>
       </main>
     )
   }
